59b2a662f9d0d306ad8b6eb62a3177f2dfe92b5c
Rex Lee <sowsow621@gmail.com>
2016-06-28 13:55:08 +1000

    Add project files.

---
+++ rEX/App_Start/RouteConfig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace rEX
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.RouteExistingFiles = true;
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "ProductInfo",
+                url: "{controller}/{productId}/{locale}",
+                defaults: new { controller = "Product", action = "Index", locale = "en-us"},
+                constraints: new { productId = @"\d[8]", locale = "[a-z]{2}-[a-z]{2}" }
+            );
+
+
+            // Register Custom Custom Route Handler
+            Route clarityRoute = new Route("clarity", new ClarityRouteHandler());
+            routes.Add("ClarityRoute", clarityRoute);
+
+        }
+    }
+
+
+    /// <summary>
+    /// Custom Route Handler Example
+    /// </summary>
+    public class ClarityRouteHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            if (requestContext.HttpContext.Request.Url.AbsolutePath == "/clarity")
+            {
+                requestContext.RouteData.Values["controller"] = "home";
+                requestContext.RouteData.Values["action"] = "clarity";
+            }
+
+            return new MvcHandler(requestContext);
+        }
+    }
+
+
+
+}
---
+++ rEX/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rEX.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("orders/{orderId}/show")]
+        public ActionResult GetOrderById(int orderId)
+        {
+            return View();
+        }
+    }
+}
